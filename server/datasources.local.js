793e0a8645b9e2e16cd8e610ec0a4758a9c26915
Luis Fernando Morales <lfmexi@gmail.com>
2015-12-03 23:31:33 -0600

    Initial commit

---
+++ server/datasources.local.js
@@ -0,0 +1,10 @@
+module.exports = {
+  investments: {
+    connector: 'mongodb',
+    hostname: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 27017,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || 'investments'
+  }
+}
