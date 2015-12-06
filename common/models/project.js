a9f4f03d2591e61685a3f1f6dc4deff0fae5c221
Luis Fernando Morales <lfmexi@gmail.com>
2015-12-06 01:36:29 -0600

    #no-issue: created the model of projects and the owner relationship

---
+++ common/models/project.js
@@ -0,0 +1,21 @@
+module.exports = function(Project) {
+
+  Project.observe('before save', function setCreatedDate (ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.created = new Date();
+    }
+    next();
+  });
+
+  Project.observe('before save', function setLastModifiedDate (ctx, next) {
+    if (ctx.instance) {
+      ctx.instance.lastModification = new Date();
+    } else {
+      ctx.data.lastModification = new Date();
+    }
+    next();
+  });
+
+  Project.disableRemoteMethod('find', true);
+
+};
