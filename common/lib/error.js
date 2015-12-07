6f718451307c44386ebcea2e86b87f2504842983
Luis Fernando Morales <lfmexi@gmail.com>
2015-12-07 01:35:22 -0600

    Cambios en el modelo de operaciones.
    Se ha cambiado el modelo de operaciones, además, se ha agregado
    la relación entre projecto y operación, además de las restricciones,
    por lo que el IU6 y el IU7 quedan cubiertos parcialmente.

---
+++ common/lib/error.js
@@ -0,0 +1,22 @@
+module.exports = {
+  getUnsuportedValueError: function getUnsuportedValue (property) {
+    return {
+      status: 422,
+      message: '422 - Value for ' + property + ' is not alowed',
+      error: '422 UNPROCESSABLE ENTITY'
+    };
+  },
+  getOptionalNotPresentError: function getNotPresent (property) {
+    return {
+      status: 422,
+      message: '422 - ' + property + ' field is expected',
+      error: '422 UNPROCESSABLE ENTITY'
+    };
+  },
+  getOperationError: function getOperation () {
+    return {
+      status: 400,
+      message: '400 - Wrong operation'
+    };
+  }
+}
--- common/models/operacion.js
+++ common/models/operacion.js
@@ -1,4 +1,64 @@
+var lmyjoType = require('lmyjo-type');
+var error = require('../lib/error');
+
+function validatePeriodic (dataSet, tipoFactor, next) {
+  if (tipoFactor === 'periodico' || tipoFactor === 'gradiente') {
+    if (!dataSet.periodicidad) {
+      return next(error.getOptionalNotPresentError('periodicidad'));
+    }
+    if (!dataSet.duracion) {
+      return next(error.getOptionalNotPresentError('duracion'));
+    }
+  }
+  if (tipoFactor === 'gradiente') {
+    if (!dataSet.incremento) {
+      return next(error.getOptionalNotPresentError('incremento'));
+    }
+  }
+  next();
+}
+
 module.exports = function(Operacion) {
+
+  Operacion.observe('before save', function tipoOperacionValidation (ctx, next) {
+    var tipoOperacion = (ctx.instance)? ctx.instance.tipo_operacion : ctx.data.tipo_operacion;
+    if (tipoOperacion) {
+      var type = lmyjoType.getTipoMovimiento(tipoOperacion);
+      if (type === null) {
+        return next(error.getUnsuportedValueError('tipo_operacion'));
+      }
+    }
+    next();
+  });
+
+  Operacion.observe('before save', function tipoFactorValidation (ctx, next) {
+    var tipoFactor = (ctx.instance)? ctx.instance.tipo_factor : ctx.data.tipo_factor;
+    if (tipoFactor) {
+      var type = lmyjoType.getTipoFactor(tipoFactor);
+      if (type === null) {
+        return next(error.getUnsuportedValueError('tipo_factor'));
+      }
+    }
+    next();
+  });
+
+  Operacion.observe('before save', function unidadTiempoValidation (ctx, next) {
+    var unidadTiempo = (ctx.instance)? ctx.instance.unidad_tiempo : ctx.data.unidad_tiempo;
+    if (unidadTiempo) {
+      var type = lmyjoType.getUnidadTiempo(unidadTiempo);
+      if (type === null) {
+        return next(error.getUnsuportedValueError('unidad_tiempo'));
+      }
+    }
+    next();
+  });
+
+  Operacion.observe('before save', function periodicValidation (ctx, next) {
+    var tipoFactor = (ctx.instance)? ctx.instance.tipo_factor : ctx.data.tipo_factor;
+    var dataSet = ctx.instance || ctx.data;
+
+    validatePeriodic(dataSet, tipoFactor, next);
+  });
 
   Operacion.observe('before save', function setFechaCreacion (ctx, next) {
     if (ctx.instance) {
@@ -7,4 +67,31 @@ module.exports = function(Operacion) {
     next();
   });
 
+  Operacion.observe('before save', function updateFechaModificacion (ctx, next) {
+    var dataSet = ctx.instance || ctx.data;
+
+    if (dataSet.project_id) {
+      var Project = Operacion.app.models.Project;
+
+      var projectId = dataSet.project_id;
+      Project.findById(projectId, function (err, project) {
+        if (err) {
+          console.log(error);
+          return next(err);
+        }
+        if (project) {
+          project.lastModification = new Date();
+          Project.upsert(project, function (err) {
+            if(err) {
+              console.log(error);
+              return next(err);
+            }
+            return next();
+          });
+        } else return next();
+      });
+    } else {
+      return next();
+    }
+  });
 };
